b7c326c1b3266cf60f6e48f07b5e790c5a9fb4a4
admin <admin@Win7_1TB>
2019-04-09 11:57:01 +0200

    Creacion de diseño y BD_diseño

--- PRY_Clothes/src/bbdd/BD_Diseño.java
+++ PRY_Clothes/src/bbdd/BD_Diseño.java
@@ -1,5 +1,13 @@
 package bbdd;
 
-public class BD_Diseño {
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class BD_Diseño extends BD_Conector {
+	private static Statement s;	
+	private static ResultSet reg;
+	
+	public BD_Diseño(){
+		super();
+	}
 }
--- PRY_Clothes/src/modelos/Diseño.java
+++ PRY_Clothes/src/modelos/Diseño.java
@@ -1,5 +1,35 @@
 package modelos;
 
+import java.time.LocalDate;
+
 public class Diseño {
+private LocalDate avisos ;
+private String descripcion;
+private int precio;
+private String categoria;
+private String codDiseño;
+public Diseño(LocalDate avisos, String descripcion, int precio, String categoria, String codDiseño) {
+	super();
+	this.avisos = avisos;
+	this.descripcion = descripcion;
+	this.precio = precio;
+	this.categoria = categoria;
+	this.codDiseño = codDiseño;
+}
+public LocalDate getAvisos() {
+	return avisos;
+}
+public String getDescripcion() {
+	return descripcion;
+}
+public int getPrecio() {
+	return precio;
+}
+public String getCategoria() {
+	return categoria;
+}
+public String getCodDiseño() {
+	return codDiseño;
+}
 
 }
