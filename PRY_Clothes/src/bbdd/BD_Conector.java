bb4be57c7e783ca359ed72261cc5964df75926ad
admin <admin@Win7_1TB>
2019-04-09 10:17:34 +0200

    Inicio del main y el BD_Conector

---
+++ PRY_Clothes/src/bbdd/BD_Conector.java
@@ -0,0 +1,50 @@
+/*
+ * BD_Conector: Se encarga de abrir y cerrar la base de datos
+ */
+package bbdd;
+
+import java.sql.*;
+
+
+public class BD_Conector {
+	static private String base;
+	static private String usuario;
+	static private String pass;
+	static private String url;
+	static protected Connection c;
+	
+	public static void BD_Ini(String bbdd){		
+		base=bbdd;
+		usuario="root";
+		pass="";
+		url="jdbc:mysql://localhost/"+base;
+	}
+	
+	public void abrir(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch (ClassNotFoundException e){
+			System.out.println(e.getMessage());
+		}
+		try{
+		 	c=DriverManager.getConnection(url,usuario,pass);
+		}
+		catch (SQLException e ){
+			System.out.println(e.getMessage());
+		}
+	
+	}	
+	
+
+	public void cerrar(){
+		try{
+			c.close();
+		}
+		catch (SQLException e){
+			System.out.println(e.getMessage());
+			
+		}
+	}
+	
+}
--- PRY_Clothes/src/principal/Main.java
+++ PRY_Clothes/src/principal/Main.java
@@ -1,10 +1,13 @@
 package principal;
 
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		
 	}
 
 }
