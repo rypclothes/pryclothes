caa241671a842879cc0658c9117a05bff42bd6cb
admin <admin@Win7_1TB>
2019-04-09 11:13:58 +0200

    Creacion de Incidencias y constructor y el BD_incidencias creado y falta el poner incidencias

--- PRY_Clothes/src/bbdd/BD_Incidencias.java
+++ PRY_Clothes/src/bbdd/BD_Incidencias.java
@@ -1,5 +1,18 @@
 package bbdd;
 
-public class BD_Incidencias {
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class BD_Incidencias {
+	private static Statement s;		
+	private static ResultSet reg;
+	
+	public BD_Incidencias() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public void ponerIncidencia(String codEmple,String tipoIncidencia) {
+		
+		
+	}
 }
