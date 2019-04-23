14f23fd600b5849e0b3677cf37139379a28b0692
admin <admin@Win7_1TB>
2019-04-23 11:24:30 +0200

    caso 1 insertar y caso 2 borrar

--- PRY_Clothes/src/bbdd/BD_Diseño.java
+++ PRY_Clothes/src/bbdd/BD_Diseño.java
@@ -1,7 +1,13 @@
 package bbdd;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
+
+import modelos.Diseño;
+
+
 
 public class BD_Diseño extends BD_Conector {
 	private static Statement s;	
@@ -10,4 +16,57 @@ public class BD_Diseño extends BD_Conector {
 	public BD_Diseño(){
 		super();
 	}
+	
+	public  int añadir_Diseño( Diseño diseños) {
+		String cadenaSQL="INSERT INTO diseños VALUES('"+diseños.getFecha_salida()+"','"+
+			   diseños.getDescripcion()+"','"+diseños.getPrecio()+"','"+diseños.getCategoria()+"','"+diseños.getCodDiseño()+"','"+diseños.getCantidad()+"')";
+		try {
+			this.abrir();
+			s=c.createStatement();
+			int filas=s.executeUpdate(cadenaSQL);
+			s.close();
+			this.cerrar();
+			return filas;
+			
+		}catch(SQLException e) {
+			return -1;
+		}
+	}
+	
+	public Vector<String> listadoCod_diseño(){
+		String cadenaSQL="SELECT cod_diseño from diseños";
+		Vector<String> listadoCod_diseño=new Vector<String>();
+		try {
+			this.abrir();
+			 s = c.createStatement();
+			 reg=s.executeQuery(cadenaSQL);
+			 while ( reg.next()){
+					listadoCod_diseño.add(reg.getString(1));
+				}	
+		}catch(SQLException e) {
+			this.cerrar();
+			System.out.println(e.getMessage());
+			return null;
+		}
+		
+		return listadoCod_diseño;
+	
+	}
+	
+	 public int borraDiseño(String cod_diseñoBorrar ) {
+		 String cadenaSQL="DELETE FROM diseños Where cod_diseño='"+cod_diseñoBorrar+"'";
+		 String tipo="";
+			try{
+				this.abrir();
+				s=c.createStatement();
+				int filas=s.executeUpdate(cadenaSQL);
+				s.close();
+				this.cerrar();
+				return filas;
+				}
+				catch ( SQLException e){			
+					return -1;
+				
+				}  
+	 }	
 }
--- PRY_Clothes/src/modelos/Diseño.java
+++ PRY_Clothes/src/modelos/Diseño.java
@@ -3,27 +3,43 @@ package modelos;
 import java.time.LocalDate;
 
 public class Diseño {
-private LocalDate avisos ;
+private LocalDate fecha_salida ;
 private String descripcion;
-private int precio;
+private double precio;
+private static  int cont;
+private  int numDiseño;
+private int cantidad;
 private String categoria;
 private String codDiseño;
-public Diseño(LocalDate avisos, String descripcion, int precio, String categoria, String codDiseño) {
+public Diseño(LocalDate fecha_salida, String descripcion, double precio, String categoria,int cantidad) {
 	super();
-	this.avisos = avisos;
+	this.fecha_salida = fecha_salida;
 	this.descripcion = descripcion;
 	this.precio = precio;
 	this.categoria = categoria;
-	this.codDiseño = codDiseño;
+	this.cont=001;
+	cont++;
+	this.codDiseño = categoria.substring(0,2).toUpperCase()+"-"+cont;
+    
+	this.cantidad=cantidad;
 }
-public LocalDate getAvisos() {
-	return avisos;
+public LocalDate getFecha_salida() {
+	return fecha_salida;
 }
 public String getDescripcion() {
 	return descripcion;
 }
-public int getPrecio() {
+public double getPrecio() {
 	return precio;
+}
+public static int getCont() {
+	return cont;
+}
+public int getNumDiseño() {
+	return numDiseño;
+}
+public int getCantidad() {
+	return cantidad;
 }
 public String getCategoria() {
 	return categoria;
@@ -31,5 +47,12 @@ public String getCategoria() {
 public String getCodDiseño() {
 	return codDiseño;
 }
+@Override
+public String toString() {
+	return "Diseño [fecha_salida=" + fecha_salida + ", descripcion=" + descripcion + ", precio=" + precio
+			+ ", numDiseño=" + numDiseño + ", cantidad=" + cantidad + ", categoria=" + categoria + ", codDiseño="
+			+ codDiseño + "]";
+}
 
 }
+
