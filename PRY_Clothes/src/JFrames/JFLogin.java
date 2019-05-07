1968268298ff060e3da5cde6d18a6ad403fe6a54
admin <admin@Win7_1TB>
2019-05-07 13:45:07 +0200

    JFrames Y JPanels

---
+++ PRY_Clothes/src/JFrames/JFLogin.java
@@ -0,0 +1,27 @@
+package JFrames;
+
+import javax.swing.*;
+
+import JPanels.*;
+import java.awt.*;
+
+public class JFLogin extends JFrame{
+	private PNLDatosUsuLogin panelLogin;
+	
+	public JFLogin() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(JFLogin.class.getResource("/imagenes/rypclothes.png")));
+		setTitle("PRY_CLOTHES");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 310, 214);
+		setLocationRelativeTo(null);
+		setResizable(true);
+		getContentPane().setLayout(null);
+		initialize();
+	}
+	
+	private void initialize() {
+		panelLogin = new PNLDatosUsuLogin();
+		getContentPane().add(panelLogin);
+	}
+}
